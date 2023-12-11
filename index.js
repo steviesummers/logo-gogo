c6908f7bc47d4161247039b6b569de43a3b74da5
steviesummers <steviesummerss@gmail.com>
2023-12-11 08:26:04 -0800

    app working, proper dev tools for jest installed

---
+++ index.js
@@ -0,0 +1,67 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+const {Circle, Square, Triangle } = require('./lib/shapes.js')
+
+function generateLogo() {
+    inquirer.prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter up to three characters for the logo:',
+          validate: function (input) {
+            return input.length <= 3;
+          },
+        },
+        {
+          type: 'input',
+          name: 'textColor',
+          message: 'Enter text color (keyword or hexadecimal):',
+        },
+        {
+          type: 'list',
+          name: 'shapeType',
+          message: 'Choose a shape:',
+          choices: ['circle', 'triangle', 'square'],
+        },
+        {
+          type: 'input',
+          name: 'shapeColor',
+          message: 'Enter shape color (keyword or hexadecimal):',
+        },
+      ])
+      .then((answers) => {
+        const {text, textColor, shapeType, shapeColor } = answers
+        
+        let shape;
+        switch (shapeType) {
+            case 'circle':
+                shape = new Circle();
+                break;
+            
+            case 'triangle': {
+                shape = new Triangle();
+                break;
+            }
+            case 'square': {
+                shape = new Square();
+                break
+            }
+            default:
+                console.error('Invalid shape type')
+                return 
+        }
+
+        shape.setText(text)
+        shape.setTextColor(textColor)
+        shape.setShapeColor(shapeColor)
+
+        const logoSVG = shape.render()
+        fs.writeFileSync('logo.svg',logoSVG)
+        console.log('generated logo')
+      })
+}
+
+generateLogo()
+
+
